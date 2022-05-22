e32da62ba755357feaf7001324fcd75f78b26ced
jjkasprzak <jjkasprzak@protonmail.com>
2022-05-22 18:40:04 +0200

    improve zooming

--- src/WindowViewController.h
+++ src/WindowViewController.h
@@ -27,16 +27,21 @@ public:
 		auto window = window_.lock();
         sf::View view = window->getView();
         const sf::Vector2f windowSize = sf::Vector2f((float)window->getSize().x, (float)window->getSize().y);
-        const sf::Vector2f mouseViewPos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y).cwiseDiv(windowSize).cwiseMul(view.getSize());
+        const sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
+        const sf::Vector2f viewULPos = sf::Vector2f(mousePos.x, mousePos.y).cwiseDiv(windowSize).cwiseMul(view.getSize());
 
         if (e.type == sf::Event::MouseWheelScrolled)
         {
-            view.zoom((e.mouseWheelScroll.delta < 0 ? 1.05f : 0.95f));
+            float factor = e.mouseWheelScroll.delta < 0 ? 1.05f : 0.95f;
+            const sf::Vector2f fromCenterPos = viewULPos - view.getSize().cwiseMul(sf::Vector2f(.5f, .5f));
+            float fixFactor = (1-factor);
+            view.move(fromCenterPos.cwiseMul({ fixFactor, fixFactor }));
+            view.zoom(factor);
         }
         if (!drag_ && e.type == sf::Event::MouseButtonPressed)
         {
             drag_ = true;
-            dragPos_ = mouseViewPos;
+            dragPos_ = viewULPos;
         }
         if (drag_ && e.type == sf::Event::MouseButtonReleased)
         {
@@ -45,8 +50,8 @@ public:
 
         if (drag_ && e.type == sf::Event::MouseMoved)
         {
-            view.move(dragPos_ - mouseViewPos);
-            dragPos_ = mouseViewPos;
+            view.move(dragPos_ - viewULPos);
+            dragPos_ = viewULPos;
         }
         window->setView(view);
 	}
